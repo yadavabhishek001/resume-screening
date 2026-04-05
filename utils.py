d0ac7df85e54558d0c0ed230c1e60c0dfef4e639
Abhishek Yadav <abhasnp55@gmail.com>
2026-04-05 10:35:46 +0530

    Implement text cleaning function
    Add clean_text function to preprocess text.

--- utils.py
+++ utils.py
@@ -8,3 +8,10 @@ def extract_text_from_pdf(file):
         text += page.extract_text()
     
     return text
+
+import re
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r'\W+', ' ', text)
+    return text
